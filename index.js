4df1f383058ab35dcf8ef909c705662e26067a84
shubhamsngwn <shubham.sangwan1104@gmail.com>
2025-02-10 18:16:33 +0530

    final commit

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ search.addEventListener("click", () => {
   )
     .then((response) => response.json())
     .then((json) => {
+      // Handling Invalid City
       if (json.cod === "404") {
         container.style.height = "400px";
         weatherBox.style.display = "none";
@@ -23,16 +24,17 @@ search.addEventListener("click", () => {
         error404.classList.add("fadeIn");
         return;
       }
+      // Hiding Error Message if City is Found
       error404.style.display = "none";
       error404.classList.remove("fadeIn");
+
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box .temperature");
       const description = document.querySelector(".weather-box .description");
-      const humidity = document.querySelector(
-        ".weather-details .humidity span"
-      );
+      const humidity = document.querySelector(".weather-details .humidity span");
       const wind = document.querySelector(".weather-details .wind span");
 
+      // Setting Weather Icon Based on Condition
       switch (json.weather[0].main) {
         case "Clear":
           image.src = "images/clear.png";
@@ -58,6 +60,7 @@ search.addEventListener("click", () => {
           image.src = "";
       }
 
+      // Updating Weather Details
       temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
       description.innerHTML = `${json.weather[0].description}`;
       humidity.innerHTML = `${json.main.humidity}%`;
